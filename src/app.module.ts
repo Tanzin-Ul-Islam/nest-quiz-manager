a60eff6d7b2119ffd146ed0fd617ca37c46b709e
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2023-05-03 00:49:37 +0600

    User entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { QuizModule } from './module/quiz/quiz.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from 'config/typeOrmConfig';
+import { ConfigModule } from '@nestjs/config';
 import { QuestionModule } from './module/question/question.module';
 import { OptionModule } from './module/option/option.module';
+import { UserModule } from './module/user/user.module';
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     QuizModule,
     QuestionModule,
     OptionModule,
+    UserModule
   ],
   controllers: [],
   providers: [],
--- src/module/user/entity/user.entity.ts
+++ src/module/user/entity/user.entity.ts
@@ -1,7 +1,16 @@
-import { Column, PrimaryGeneratedColumn } from "typeorm";
-export class User{
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, Entity, BaseEntity } from "typeorm";
+@Entity('user')
+export class User extends BaseEntity{
     @PrimaryGeneratedColumn()
     id: number;
     @Column({type:'varchar'})
     name: string;
+    @Column({unique: true})
+    email: string;
+    @Column({type:'varchar'})
+    password: string;
+    @CreateDateColumn()
+    createdAt: Date;
+    @CreateDateColumn()
+    updatedAt: Date;
 }
